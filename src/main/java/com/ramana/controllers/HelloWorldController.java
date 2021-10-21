eae8e0faa8c5acc6b9038abab5f950ffa0a6b951
Ramana R. A <ramana.prep@gmail.com>
2021-10-21 10:52:57 +0530

    updated hello world rest service with bean

--- src/main/java/com/ramana/controllers/HelloWorldController.java
+++ src/main/java/com/ramana/controllers/HelloWorldController.java
@@ -1,17 +1,25 @@
 package com.ramana.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ramana.pojo.HelloWorld;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(method=RequestMethod.GET,path="/Hello-World")
+	@RequestMapping(method = RequestMethod.GET, path = "/Hello-World")
 	public String hellowWorld() {
 
 		return "Hello World !!!";
+	}
+
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorld helloWorldBean() {
+
+		return new HelloWorld("Hello World From Bean!!!!!!");
 	}
 
 }
